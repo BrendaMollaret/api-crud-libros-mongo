3d2f12ea8d2a4174682c0ab165239d8b909eb0db
Brenda Mollaret <mollaretbrenda@gmail.com>
2026-06-29 21:20:20 -0300

    feat libro model

---
+++ models/libromodel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+//Definir el esquema del libro
+const libroSchema = new mongoose.Schema(
+  {
+    titulo: { type: String, required: true },
+    autor: { type: String, required: true },
+    categoria: { type: String, required: true },
+    estado: {
+      type: String,
+      enum: ["disponible", "prestado", "vencido"],
+      default: "disponible",
+    },
+    fechaPrestamo: { type: Date },
+    fechaDevolucion: { type: Date },
+  },
+  { timestamps: true }, // Agregar timestamps para createdAt y updatedAt
+);
+
+// Crear el modelo del libro
+const ModelLibro = mongoose.model("libros", libroSchema);
+module.exports = ModelLibro;
